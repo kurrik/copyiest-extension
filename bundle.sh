5b666e55752c9ef08e3970f97c1a4713b2dc4736
Arne Roomann-Kurrik <kurrik@gmail.com>
2023-02-07 22:22:05 -0800

    Dev/prod builds

--- bundle.sh
+++ bundle.sh
@@ -3,5 +3,12 @@ set -e
 
 ROOT=$(git rev-parse --show-toplevel)
 
-cd $ROOT/src
+cd $ROOT
+
+mkdir -p dist
+cp -r src/* dist/
+cp -r prod-config/images/* dist/images/
+jq --slurp add src/manifest.json prod-config/manifest.json > dist/manifest.json
+
+cd dist
 zip -r ../copiest-bundle.zip *
